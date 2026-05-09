3cffc68c9109a88a9c0db546fcbf7186c28e3c63
Hoang Phi <phi.do@donuts-vn.com>
2026-05-09 20:18:31 +0700

    OSP 24 improve disable when stock = 0

--- frontend/src/components/ui/productDescriptionDetail/ProductActions.jsx
+++ frontend/src/components/ui/productDescriptionDetail/ProductActions.jsx
@@ -5,15 +5,23 @@ import Button from "../button/Button";
 
 const cx = classNames.bind(styles);
 
-const ProductActions = ({ onAddToCart, onBuyNow }) => {
+const ProductActions = ({ onAddToCart, onBuyNow, isOutOfStock }) => {
   const { t } = useTranslation();
 
   return (
     <div className={cx("action-buttons")}>
-      <Button className={cx("add-to-cart-button")} onClick={onAddToCart}>
+      <Button
+        className={cx("add-to-cart-button")}
+        onClick={onAddToCart}
+        disabled={isOutOfStock}
+      >
         {t("components.ui.product.actions.add_to_cart")}
       </Button>
-      <Button className={cx("buy-now-button")} onClick={onBuyNow}>
+      <Button
+        className={cx("buy-now-button")}
+        onClick={onBuyNow}
+        disabled={isOutOfStock}
+      >
         {t("components.ui.product.actions.buy_now")}
       </Button>
     </div>
